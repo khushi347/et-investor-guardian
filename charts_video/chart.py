76ea59e631fa0f0c5506acc8ca03bb27fd546cef
Khushi <khushi240305@gmail.com>
2026-03-20 20:56:27 +0530

    M4: Added stock chart (TCS/RELIANCE) and voice generation using gTTS

---
+++ charts_video/chart.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import plotly.express as px
+import pandas as pd
+
+def plot_stock(stock_symbol):
+    data = yf.download(stock_symbol, period="1mo", interval="1d")
+
+    # 🔥 FIX: flatten multi-level columns
+    data.columns = data.columns.get_level_values(0)
+
+    data = data.reset_index()
+    data['Date'] = pd.to_datetime(data['Date'])
+
+    fig = px.line(
+        data,
+        x='Date',
+        y='Close',
+        title=f"{stock_symbol} - Last 1 Month Trend"
+    )
+
+    fig.update_layout(
+        template="plotly_dark",
+        height=500
+    )
+
+    fig.update_yaxes(tickprefix="₹")
+
+    return fig
